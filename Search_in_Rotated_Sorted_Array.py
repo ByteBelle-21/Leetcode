072d1a87d04ce78b936b751acb06604d5dd66542
Prajakta Sanjay Sutar <jrg814@usask.ca>
2026-03-11 11:22:42 -0600

    Create  Search_in_Rotated_Sorted_Array.py

---
+++ Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,26 @@
+# Leetcode question = https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) -1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if  (nums[left] <= target) and (target < nums[mid]):
+                    right = mid - 1
+                else:
+                    left = mid +1 
+            
+            if nums[right] >= nums[mid]:
+                if  (nums[right] >= target) and (target > nums[mid]):
+                    left = mid + 1
+                else:
+                    right = mid -1 
+            
+        return -1
