d9a24979b9aafa95365b9a62c30198c76e7ab343
Prajakta Sanjay Sutar <jrg814@usask.ca>
2026-03-11 11:01:45 -0600

    Create Generate_Parentheses.py

---
+++ Generate_Parentheses.py
@@ -0,0 +1,19 @@
+# Leetcode Question  -  https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result  = []
+
+        def backtrack_generate(s, open_p, close_p):
+            if len(s) == (2*n):
+                result.append(s)
+                return
+            
+            if open_p < n:
+                backtrack_generate((s+"("), open_p+1, close_p)
+
+            if open_p > close_p:
+                backtrack_generate((s+")"), open_p, close_p+1)
+        
+        backtrack_generate("", 0, 0)
+        return result
